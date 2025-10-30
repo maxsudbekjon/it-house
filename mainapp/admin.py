012d7827e50f7dd769dc89fce744173c0a166bef
shohjahonrav <ravshanovshohjahon750@gmail.com>
2025-10-30 21:58:13 +0500

    .

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Course, Technology, CourseModule, ModuleTheme, Statistics, Company, Teacher, TeacherAchievement, TeacherSkill
+
 
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(Technology)
+admin.site.register(CourseModule)
+admin.site.register(ModuleTheme)
+admin.site.register(Statistics)
+admin.site.register(Company)
+admin.site.register(Teacher)
+admin.site.register(TeacherAchievement)
+admin.site.register(TeacherSkill)
