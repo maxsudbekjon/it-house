c0076311720b40d8220f8089c56676295cbeba88
Sherzod <105229066+Rohatov@users.noreply.github.com>
2025-11-11 15:33:28 +0500

    Updated

--- mainapp/models.py
+++ mainapp/models.py
@@ -20,7 +20,7 @@ class Course(BaseModel):
     description_ru = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     duration = models.IntegerField(help_text="Duration in months")
-    students = models.PositiveIntegerField(default=0)
+    students = models.PositiveIntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return self.title_uz
