3b0722bfa7bb50336d14307f2ad79b05931e2a43
Sherzod Rohatov <srohatov1@gmail.com>
2025-11-20 17:22:12 +0500

    Updated

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,21 +1,36 @@
 from django.urls import path
-from mainapp.views import (CourseListView, TopStatisticsListView,
-                            MediumStatisticsListView, TeacherListView,
-                            CourseDetailView, NewsListView, CompanyListView,
-                            TeacherDetailView, EducationAboutView, CourseAboutView)
-
+from mainapp.views import (
+    TopStatisticsListView,
+    MediumStatisticsListView,
+    NewsListView,
+    CompanyListView,
+    EducationAboutView,
+    CourseAboutView,
+    CourseAPIView,
+    TechnologyAPIView,
+    CourseModuleAPIView,
+    ModuleThemeAPIView,
+    TeacherAPIView,
+    TeacherAchievementAPIView,
+    TeacherSkillAPIView,
+)
 from rest_framework.routers import DefaultRouter
+
 router = DefaultRouter()
-router.register(r'courses', CourseDetailView, basename='course')
-router.register(r'top-statistics', TopStatisticsListView, basename='top-statistics')
-router.register(r'medium-statistics', MediumStatisticsListView, basename='medium-statistics')
-router.register(r'teachers', TeacherListView, basename='teachers')
-router.register(r'teachers', TeacherDetailView, basename='teacher-detail')
-router.register(r'news', NewsListView, basename='news')
-router.register(r'companies', CompanyListView, basename='companies')
-router.register(r'education-about', EducationAboutView, basename='education-about')
-router.register(r'course-about', CourseAboutView, basename='course-about')  
+
+router.register('top-statistics', TopStatisticsListView, basename='top-statistics')
+router.register('medium-statistics', MediumStatisticsListView, basename='medium-statistics')
+router.register('news', NewsListView, basename='news')
+router.register('company', CompanyListView, basename='company')
+router.register('education-about', EducationAboutView, basename='education-about')
+router.register('course-about', CourseAboutView, basename='course-about')
+router.register('courses', CourseAPIView, basename='courses')
+router.register('teachers', TeacherAPIView, basename='teachers')
 
 urlpatterns = [
-    path('course-list/', CourseListView.as_view(), name='course-list'),
+    path('technologies/<int:course_id>/', TechnologyAPIView.as_view(), name='technologies'),
+    path('course-modules/<int:course_id>/', CourseModuleAPIView.as_view(), name='course-modules'),
+    path('module-themes/<int:module_id>/', ModuleThemeAPIView.as_view(), name='module-themes'),
+    path('teacher-achievements/<int:teacher_id>/', TeacherAchievementAPIView.as_view(), name='teacher-achievements'),
+    path('teacher-skills/<int:teacher_id>/', TeacherSkillAPIView.as_view(), name='teacher-skills'),
 ] + router.urls
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
-from rest_framework.viewsets import ReadOnlyModelViewSet, GenericViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet, GenericViewSet, ModelViewSet
 from rest_framework.mixins import RetrieveModelMixin, ListModelMixin
 from mainapp.serializers import (CourseSerializer, TechnologySerializer,
                                  CourseModuleSerializer, ModuleThemeSerializer,
@@ -11,68 +12,180 @@ from mainapp.serializers import (CourseSerializer, TechnologySerializer,
                                 CourseAboutSerializer)
 from mainapp.models import (Course, Technology, CourseModule, ModuleTheme, Company, EducationAbout,
                             Statistics, Teacher, TeacherAchievement, TeacherSkill, News, CourseAbout)
+from rest_framework.response import Response
+from rest_framework import status
 
 
-
-class CourseListView(ListAPIView):
-    queryset = Course.objects.all()
-    serializer_class = CourseListSerializer
-
-class CourseDetailView(RetrieveModelMixin, GenericViewSet):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-
-
-class TechnologyListView(ReadOnlyModelViewSet):
-    queryset = Technology.objects.all()
-    serializer_class = TechnologySerializer
-
-
-class CourseModuleListView(ReadOnlyModelViewSet):
-    queryset = CourseModule.objects.all()
-    serializer_class = CourseModuleSerializer
-
-
-class ModuleThemeListView(ReadOnlyModelViewSet):
-    queryset = ModuleTheme.objects.all()
-    serializer_class = ModuleThemeSerializer
-
-
-class TopStatisticsListView(ListModelMixin, GenericViewSet):
+class TopStatisticsListView(ModelViewSet):
     queryset = Statistics.objects.all()
     serializer_class = TopStatisticsSerializer
 
 
-class MediumStatisticsListView(ListModelMixin, GenericViewSet):
+class MediumStatisticsListView(ModelViewSet):
     queryset = Statistics.objects.all()
     serializer_class = MediumStatisticsSerializer
 
 
-class TeacherListView(ListModelMixin, GenericViewSet):
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherListSerializer
-
-
-class TeacherDetailView(RetrieveModelMixin, GenericViewSet):
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherSerializer
-
-
-class NewsListView(ReadOnlyModelViewSet):
+class NewsListView(ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
 
 
-class CompanyListView(ListModelMixin, GenericViewSet):
+class CompanyListView(ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
 
-class EducationAboutView(ListModelMixin, GenericViewSet):
+class EducationAboutView(ModelViewSet):
     queryset = EducationAbout.objects.all()
     serializer_class = EducationAboutSerializer
 
 
-class CourseAboutView(ListModelMixin, GenericViewSet):
+class CourseAboutView(ModelViewSet):
     queryset = CourseAbout.objects.all()
     serializer_class = CourseAboutSerializer
+
+
+class CourseAPIView(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    
+
+class TechnologyAPIView(APIView):
+    serializer_class = TechnologySerializer
+    
+    def post(self, request, course_id):
+        course = get_object_or_404(Course, id=course_id)
+        serializer = self.serializer_class(data=request.data, context = {'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save(course=course)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def patch(self, request, course_id):
+        course = get_object_or_404(Course, id=course_id)
+        technology_id = request.data.get('id')
+        technology = get_object_or_404(Technology, id=technology_id, course=course)
+        serializer = self.serializer_class(technology, data=request.data, context = {'request': request}, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(course=course)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, course_id):
+        course = get_object_or_404(Course, id=course_id)
+        tech_id = request.data.get('id')
+        technology = get_object_or_404(Technology, id=tech_id, course=course)
+        technology.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+class CourseModuleAPIView(APIView):
+    serializer_class = CourseModuleSerializer
+    
+    def post(self, request, course_id):
+        course = get_object_or_404(Course, id=course_id)
+        serializer = self.serializer_class(data=request.data, context = {'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save(course=course)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def patch(self, request, course_id):
+        course = get_object_or_404(Course, id=course_id)
+        module_id = request.data.get('id')
+        module = get_object_or_404(CourseModule, id=module_id, course=course)
+        serializer = self.serializer_class(module, data=request.data, context = {'request': request}, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(course=course)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, course_id):
+        course = get_object_or_404(Course, id=course_id)
+        module_id = request.data.get('id')
+        module = get_object_or_404(CourseModule, id=module_id, course=course)
+        module.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+class ModuleThemeAPIView(APIView):
+    serializer_class = ModuleThemeSerializer
+    
+    def post(self, request, module_id):
+        module = get_object_or_404(CourseModule, id=module_id)
+        serializer = self.serializer_class(data=request.data, context = {'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save(module=module)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def patch(self, request, module_id):
+        module = get_object_or_404(CourseModule, id=module_id)
+        theme_id = request.data.get('id')
+        theme = get_object_or_404(ModuleTheme, id=theme_id, module=module)
+        serializer = self.serializer_class(theme, data=request.data, context = {'request': request}, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(module=module)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, module_id):
+        module = get_object_or_404(CourseModule, id=module_id)
+        theme_id = request.data.get('id')
+        theme = get_object_or_404(ModuleTheme, id=theme_id, module=module)
+        theme.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class TeacherAPIView(ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+
+class TeacherAchievementAPIView(APIView):
+    serializer_class = TeacherAchievementSerializer
+    
+    def post(self, request, teacher_id):
+        teacher = get_object_or_404(Teacher, id=teacher_id)
+        serializer = self.serializer_class(data=request.data, context = {'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save(teacher=teacher)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def patch(self, request, teacher_id):
+        teacher = get_object_or_404(Teacher, id=teacher_id)
+        achievement_id = request.data.get('id')
+        achievement = get_object_or_404(TeacherAchievement, id=achievement_id, teacher=teacher)
+        serializer = self.serializer_class(achievement, data=request.data, context = {'request': request}, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(teacher=teacher)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, teacher_id):
+        teacher = get_object_or_404(Teacher, id=teacher_id)
+        achievement_id = request.data.get('id')
+        achievement = get_object_or_404(TeacherAchievement, id=achievement_id, teacher=teacher)
+        achievement.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class TeacherSkillAPIView(APIView):
+    serializer_class = TeacherSkillSerializer
+    
+    def post(self, request, teacher_id):
+        teacher = get_object_or_404(Teacher, id=teacher_id)
+        serializer = self.serializer_class(data=request.data, context = {'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save(teacher=teacher)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def patch(self, request, teacher_id):
+        teacher = get_object_or_404(Teacher, id=teacher_id)
+        skill_id = request.data.get('id')
+        skill = get_object_or_404(TeacherSkill, id=skill_id, teacher=teacher)
+        serializer = self.serializer_class(skill, data=request.data, context = {'request': request}, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(teacher=teacher)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, teacher_id):
+        teacher = get_object_or_404(Teacher, id=teacher_id)
+        skill_id = request.data.get('id')
+        skill = get_object_or_404(TeacherSkill, id=skill_id, teacher=teacher)
+        skill.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
