307b70f2f7e7a6762100563426ee357c6157bf46
Sherzod Rohatov <srohatov1@gmail.com>
2025-11-25 16:36:03 +0500

    Login API

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path,include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
@@ -12,4 +16,6 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/', include('mainapp.urls')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
