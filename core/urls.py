cdf83d72ca9d1874f3c8180100c39d15bc2a7cee
ChoriyevJamshid <jamshidchoriyev709@gmail.com>
2026-01-16 11:05:37 +0500

    .

--- core/urls.py
+++ core/urls.py
@@ -11,12 +11,15 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-page-locked/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api-schema-locked', SpectacularAPIView.as_view(), name='schema'),
+    path('api-swagger-locked', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/', include('mainapp.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/podcasts/', include('podcasts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
