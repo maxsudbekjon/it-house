ac2e663efb0e572df660b762c5af8c35e9cb016c
ChoriyevJamshid <jamshidchoriyev709@gmail.com>
2026-01-17 09:05:36 +0500

    .

--- core/urls.py
+++ core/urls.py
@@ -13,8 +13,8 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin-page-locked/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api-schema-locked', SpectacularAPIView.as_view(), name='schema'),
-    path('api-swagger-locked', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api-schema-locked/', SpectacularAPIView.as_view(), name='schema'),
+    path('api-swagger-locked/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/', include('mainapp.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
