a939ecb1b1001e63f219bb0487ba205437553b21
ChoriyevJamshid <jamshidchoriyev709@gmail.com>
2026-01-17 09:43:08 +0500

    .

--- mainapp/models.py
+++ mainapp/models.py
@@ -164,7 +164,7 @@ class EducationAbout(BaseModel):
 class ContactMessage(models.Model):
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.CharField(max_length=63, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
